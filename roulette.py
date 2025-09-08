7c3b667b5060a02d0128c4d7e00ad5c774e0e09f
veer-faculty <veer.gala2@faculty.ai>
2025-09-08 11:33:35 +0100

    Inital working roulette

---
+++ roulette.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+from pydantic_ai import Agent, RunContext
+
+load_dotenv()
+
+model_choice = "openai:gpt-4o"
+
+roulette_agent = Agent(
+    model=model_choice,
+    deps_type=int,
+    output_type=bool,
+    system_prompt=(
+        "Use the `roulette_wheel` function to see if the "
+        "customer has won based on the number they provide."
+    ),
+)
+
+
+@roulette_agent.tool
+async def roulette_wheel(ctx: RunContext[int], square: int) -> str:
+    """check if the square is a winner"""
+    return "winner" if square == ctx.deps else "loser"
+
+
+# Run the agent
+success_number = 18
+result = roulette_agent.run_sync("Put my money on square eighteen", deps=success_number)
+print(result.output)
+# > True
+
+result = roulette_agent.run_sync("I bet five is the winner", deps=success_number)
+print(result.output)
+# > False
